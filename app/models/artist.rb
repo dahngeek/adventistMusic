e44167d98f4b0f8bc5120674ee585cfb5b09ea90
Daniel Xutuc <danielxutuc@gmail.com>
2019-06-23 19:46:34 -0300

    relationships in the models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,5 @@
 class Artist < ApplicationRecord
+    has_and_belongs_to_many :songs
+    has_and_belongs_to_many :albums
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :type
+  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :artists
+  has_and_belongs_to_many :playlists
+
 end
