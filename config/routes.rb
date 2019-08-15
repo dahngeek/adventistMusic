68eb217bb6b16129bd722271d5252ad4050c2a4d
Daniel Xutuc <danielxutuc@gmail.com>
2019-08-15 15:57:15 -0300

    updates for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/artista/:id', to: 'pages#artist', as: 'artist'
   get '/valbum/:id', to: 'pages#album', as: 'album'
   get '/albumes', to: 'pages#albums_list'
-  get '/buscar/', to: 'pages#search', :defaults => { :term => '' }
+  get '/buscar/', to: 'pages#search'
   get '/cancion/:id/play' => 'pages#play', :defaults => { :type => 'song' }
   get '/valbum/:id/play' => 'pages#play', :defaults => { :type => 'album' }
   get '/artista/:id/play' => 'pages#play', :defaults => { :type => 'artist' }
